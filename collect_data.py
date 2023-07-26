683ca779cbe86785b4d257ba02c5451b6569d841
Evan Toomey <etoomey81@gmail.com>
2023-07-26 19:27:30 -0400

    data is now saved in json

--- collect_data.py
+++ collect_data.py
@@ -1,233 +1,113 @@
+import time
+start = time.time()
+import threading
 import spotipy
 from spotipy import SpotifyOAuth
-import random
-import pandas as pd
-import matplotlib.pyplot as plt
 from collections import Counter
-from pprint import pprint  # noqa
-import time
-import numpy as np
-
-divider = "|"
-start = time.time()
+from pprint import pprint # noqa
+import json
 
 
-def format_name(name):
-    name = name.split(" (")[0]
-    name = name.split(" - ")[0]
-    name = name.split(" / ")[0]
-    name = name.split(" Remastered")[0]
-    while name[-1] in [" ", "/", "\\"]:
-        name = name[:-1]
-    return name
+def format_song_name(song_name):
+	song_name = song_name.split(" (")[0]
+	song_name = song_name.split(" - ")[0]
+	song_name = song_name.split(" / ")[0]
+	song_name = song_name.split(" Remastered")[0]
+	while song_name[-1] in [" ", "/", "\\"]:
+		song_name = song_name[:-1]
+	return song_name
 
 
 def iterations(length, max_request=50):
-    if length % max_request == 0:
-        return int((length/50)-1)
-    return int(str(length/50).split(".")[0])
+	if length % max_request == 0:
+		return int((length/50)-1)
+	return int(str(length/50).split(".")[0])
+
+def format_data(song_list):
+	song_list = dict(song_list)["items"]
+	for i in range(len(song_list)):
+		added = song_list[i]["added_at"]
+		song_list[i] = dict(song_list[i]["track"])
+		song_list[i]["added_at"] = added
+		song_list[i]["formatted_name"] = format_song_name(song_list[i]["name"])
+	return song_list
 
 
-def format_data(track):
-    raw_name = track["name"]
-    if raw_name == "Stop":
-        print("Program Force Stopped")
-        return "error"
-    artists = {artist["name"]: artist["id"] for artist in track["artists"]}
-    duration = track["duration_ms"]
-    track_id = track["id"]
-    explicit = track["explicit"]
-    formatted_name = format_name(raw_name)
-    release_date = int(dict(track["album"])["release_date"][:4])
-    if "dict" not in str(type(track)):
-        return "error"
-    track_data = "{}|{}|{}|{}|{}|{}|{}\n".format(
-        raw_name, artists, duration, track_id, explicit, formatted_name, release_date)
-    return track_data
+def get_all_songs(spotify_client):
+	with open("tracks.json") as data_file:
+		data_file.write("")
+	raw_data = dict(spotify_client.current_user_saved_tracks(
+		limit=50, market="US", offset=0))
+	playlist_len = int(raw_data["total"])
+	data_list = format_data(raw_data)
+	for i in range(iterations(playlist_len)):
+		offset = i*50+51
+		raw_data = dict(spotify_client.current_user_saved_tracks(
+			limit=50, market="US", offset=offset))
+		data_list.extend(format_data(raw_data))
+	with open("tracks.json", "a") as data_file:
+		json.dump({"data":data_list}, data_file)
+
+def check_for_new_songs(spotify_client):
+	with open("tracks.json") as data_file:
+		data = dict(json.load(data_file))["data"]
+	saved_song_ids = []
+	for song in data:
+		saved_song_ids.append(song["id"])
+	saved_song_ids = list(set(saved_song_ids))
 
 
-def write_track(track):
-    with open(r"C:\\Users\etoom\python files\spotipy\track_data.txt", "a") as data_file:
-        data_file.write(track)
-
-
-def get_all_songs(sp):
-    with open(r"C:\\Users\etoom\python files\spotipy\track_data.txt", "w") as data_file:  # noqa
-        pass
-    raw_data = dict(sp.current_user_saved_tracks(
-        limit=50, market="US", offset=0))
-    playlist_len = int(raw_data["total"])
-    record_data(raw_data)
-    for i in range(iterations(playlist_len)):
-        offset = i*50+51
-        raw_data = dict(sp.current_user_saved_tracks(
-            limit=50, market="US", offset=offset))
-        if record_data(raw_data) == "error":
-            return
-
-
-def record_data(raw):
-    raw = [dict(dict(i)["track"]) for i in raw["items"]]
-    for i in raw:
-        data = format_data(dict(i))
-        if data == "error":
-            return "error"
-        write_track(data)
-    return
-
-
-def get_genres(sp, divider):
-    with open(r"C:\\Users\etoom\python files\spotipy\track_data.txt") as data_file:
-        data = data_file.readlines()
-    data = [list(eval(track.split(divider)[1]).values()) for track in data]
-    artist_ids = []
-    for id in data:
-        if "list" in str(type(id)):
-            for i in id:
-                artist_ids.append(i)
-        else:
-            artist_ids.append(id)
-    artist_ids = list(set(artist_ids))
-    genres = []
-    for i in range(iterations(len(artist_ids))):
-        batch_genres = dict(sp.artists(artist_ids[i*50:(i+1)*50]))
-        for artist in batch_genres["artists"]:
-            genres.extend(artist["genres"])
-    genre_dict = str(dict(Counter(genres)))
-    with open(r"C:\\Users\etoom\python files\spotipy\genres.txt", "w") as data_file:
-        data_file.writelines(genre_dict)
-
-
-def read_data(divider):
-    with open(r"C:\\Users\etoom\python files\spotipy\track_data.txt") as data_file:
-        raw_data = data_file.readlines()
-    df = pd.DataFrame({
-        "raw_name": [],
-        "duration": [],
-        "artists": [],
-        "ID": [],
-        "explicit": [],
-        "formatted_name": [],
-        "release_date": []
-    })
-    for i in raw_data:
-        if i.count(divider) != 6:
-            print("Execution Failed")
-            print("Change Divider")
-            print(f" Divider count: {i.count(divider)}")
-            print(i)
-            return
-        item = i.split(divider)
-        raw_name = item[0]
-        artists = item[1]
-        duration = int(int(item[2])/1000)
-        ID = item[3]
-        explicit = item[4]
-        formatted_name = item[5]
-        release_date = int(item[6][:-1])
-        df.loc[len(df.index)] = ({
-            "raw_name": raw_name,
-            "duration": duration,
-            "artists": artists,
-            "ID": ID,
-            "explicit": explicit,
-            "formatted_name": formatted_name,
-            "release_date": release_date
-        })
-    # length = len(df)
-    # df.drop_duplicates(inplace=True)
-    # if length > len(df):
-    #     print("There were "+str(length-len(df))+" copies")
-    return df
-
-
-def remaster(sp, data):
-    length = len(data)
-    with open(r"C:\\Users\etoom\python files\spotipy\track_data.txt") as data_file:
-        raw_data = data_file.readlines()
-
-    def release_date(i): return int(dict(i["album"])["release_date"][:4])
-    for i in range(len(data)):
-        name = data["formatted_name"][i].lower()
-        name = name[0:len(name) if len(name)<30 else 30]
-        artist = list(eval(data["artists"][i]).keys())[0].lower()
-        q = f"artist:'{artist}' track:'{name}'"
-        try:
-            results = list(dict(sp.search(q=q.encode(), type="track", market="US")["tracks"])["items"])
-        except:
-            continue
-        if results == []:
-            continue
-        first_release = release_date(results[0])
-        for j in results:
-            j = dict(j)
-            if release_date(j) < first_release:
-                first_release = release_date(j)
-        match = int(len(data["raw_name"][i])*.5)
-        if data["raw_name"][i][match] != raw_data[i][match] or first_release < 1920:
-            print(data["raw_name"][i][match], raw_data[i][match])
-            continue
-        raw_data[i] = raw_data[i][:-5]+f"{first_release}\n"
-        if "str" not in str(type(raw_data[i])):
-            print(str(type(raw_data[i]))+" oops")
-            print(raw_data[i])
-            return
-        print(str(round((i/length)*100, 1))+"%")
-    print("100.0%")
-    with open(r"C:\\Users\etoom\python files\spotipy\track_data.txt", "w") as data_file:
-        data_file.writelines(raw_data)
+def get_genres(spotify_client):
+	with open("genres.json", "w") as data_file:
+		data_file.write("")
+	with open("tracks.json") as data_file:
+		data = dict(json.load(data_file))["data"]
+	genre_list = []
+	artist_ids = []
+	for song in data:
+		for artist in song["artists"]:
+			artist_ids.append(artist["id"])
+	artist_ids = list(set(artist_ids))
+	for i in range(iterations(len(artist_ids))):
+		batch_genres = dict(spotify_client.artists(artist_ids[i*50:(i+1)*50]))
+		for artist in batch_genres["artists"]:
+			genre_list.extend(artist["genres"])
+	genre_dict = dict(Counter(genre_list))
+	with open("genres.json", "w") as data_file:
+		json.dump(genre_dict, data_file)
 
 
 def podcasts(sp):
-    podcast_strings=[]
-    max_podcast_request = True
-    raw_podcasts = []
-    n = 0
-    while max_podcast_request:
-        new_podcast_request = dict(sp.current_user_saved_episodes(offset = n))["items"]
-        raw_podcasts.extend(new_podcast_request)
-        if len(new_podcast_request)<20:
-            max_podcast_request = False
-        n+=20
-    name=""
-    show=""
-    duration=""
-    podcast={}
-    for i in raw_podcasts:
-        podcast = dict(i["episode"])
-        name=podcast["name"]
-        show=dict(podcast["show"])["name"]
-        duration=int(podcast["duration_ms"]/1000)
-        podcast_strings.append(f"{show}/{duration}/{name}\n")
-    for i in range(len(podcast_strings)):
-        podcast_strings[i] = podcast_strings[i].encode("ASCII", "ignore").decode("ASCII")
-    with open(r"C:\\Users\etoom\python files\spotipy\podcast_data.txt", "w") as podcast_file:
-        podcast_file.writelines(podcast_strings)
+	with open("podcasts.json", "w") as data_file:
+		data_file.write("")
+	max_podcast_request = True
+	podcasts = []
+	n = 0
+	while max_podcast_request:
+		new_podcast_request = dict(sp.current_user_saved_episodes(offset = n))["items"]
+		podcasts.extend(new_podcast_request)
+		if len(new_podcast_request)<20:
+			max_podcast_request = False
+		n+=20
+	with open("podcasts.json", "w") as podcast_file:
+		json.dump(podcasts, podcast_file)
 
 
 ID = "b95f6a39d55e4ee4bb0a1e7e64ccaf2b"
-SECRET = "828dbd9fd66b4c7b9041257c14c22e02"
-divider = "|"
+SECRET = "c14a3bf94565468fa97c2ebd76c73757"
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=ID, client_secret=SECRET,
-    redirect_uri="http://www.evantoomey.com/", scope="user-library-read"))
+spotify_client = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=ID, client_secret=SECRET,
+	redirect_uri="http://www.evantoomey.com/", scope="user-library-read"))
 
 
-for i in range(10):
-    print(" ")
+# threading.Thread(target=podcasts, args=(spotify_client,)).start()
+get_all_songs(spotify_client)
+# get_genres(spotify_client)
 
-print("Would you like to get accurate release dates for of all your liked songs?(y/n)")
-print("It should take 4-7 mins.")
-get_dates = str(input()) == "y"
-if get_dates:
-    remaster(sp, read_data(divider))
-get_genres(sp, divider)
-podcasts(sp)
-get_all_songs(sp)
 
 total_time = round(time.time()-start, 0)
 if total_time<=60:
-    print("Completed successfully in "+str(int(total_time % 60))+" seconds")
+	print("Completed successfully in "+str(int(total_time % 60))+" seconds")
 else:
-    print("Completed successfully in "+str(int(total_time/60)) +
-          " minutes and "+str(int(total_time % 60))+" seconds")
+	print("Completed successfully in "+str(int(total_time/60)) +
+		" minutes and "+str(int(total_time % 60))+" seconds")
--- use_data.py
+++ use_data.py
@@ -1,230 +1,227 @@
-from textwrap import wrap
-from spotipy import SpotifyOAuth
 import pandas as pd
 from matplotlib import pyplot as plt
 from collections import Counter
-from pprint import pprint  # noqa
-import time
+from pprint import pprint
 import numpy as np
 
-divider = "|"
+divider = "<divide>"
 
 
-def read_data(divider):
-    with open(r"C:\\Users\etoom\python files\spotipy\track_data.txt") as data_file:
-        raw_data = data_file.readlines()
-    df = pd.DataFrame({
-        "raw_name": [],
-        "duration": [],
-        "artists": [],
-        "ID": [],
-        "explicit": [],
-        "formatted_name": [],
-        "release_date": []
-    })
-    for i in raw_data:
-        if i.count(divider) != 6:
-            print("Execution Failed")
-            print("Change Divider")
-            print(f" Divider count: {i.count(divider)}")
-            print(i)
-            return
-        item = i.split(divider)
-        raw_name = item[0]
-        artists = item[1]
-        duration = int(round(int(item[2])/1000, 0))
-        ID = item[3]
-        explicit = item[4]
-        formatted_name = item[5]
-        release_date = int(item[6][:-1])
-        df.loc[len(df.index)] = ({
-            "raw_name": raw_name,
-            "duration": duration,
-            "artists": artists,
-            "ID": ID,
-            "explicit": explicit,
-            "formatted_name": formatted_name,
-            "release_date": release_date
-        })
-    length = len(df)
-    duplicate_indexes = df.loc[df.duplicated(), :]
-    df.drop_duplicates(inplace=True)
-    if length > len(df):
-        print("There were "+str(length-len(df))+" copies")
-        pprint(duplicate_indexes)
-    return df
+def read_spotify_data(divider):
+	with open("track_data.txt") as data_file:
+		raw_data = data_file.readlines()
+	df = pd.DataFrame({
+		"raw_name": [],
+		"duration": [],
+		"artists": [],
+		"ID": [],
+		"explicit": [],
+		"formatted_name": [],
+		"release_date": []
+	})
+	for i in raw_data:
+		if i.count(divider) != 6:
+			print("Execution Failed")
+			print("Change Divider")
+			print(f" Divider count: {i.count(divider)}")
+			print(i)
+			return
+		item = i.split(divider)
+		raw_name = item[0]
+		artists = str(list(eval(item[1]).keys()))
+		duration = int(round(int(item[2])/1000, 0))
+		ID = item[3]
+		explicit = item[4]
+		formatted_name = item[5]
+		release_date = int(item[6][:-1])
+		df.loc[len(df.index)] = ({ # type: ignore
+			"raw_name": raw_name,
+			"duration": duration,
+			"artists": artists,
+			"ID": ID,
+			"explicit": explicit,
+			"formatted_name": formatted_name,
+			"release_date": release_date
+		})
+	length = len(df)
+	duplicate_indexes = df.loc[df.duplicated(), :]
+	df.drop_duplicates(inplace=True)
+	if length > len(df):
+		print(f"There {'were' if (length-len(df))>1 else 'was'} "+str(length-len(df))+f" duplicate{'s' if (length-len(df))>1 else ''}")
+		pprint(duplicate_indexes)
+	return df
 
 
 def duration_graph_organization(df, bars_per_graph):
-    df.sort_values(by=["duration"], ascending=True, inplace=True)
-    avg_track = int((round(df["duration"].sum()/len(df["duration"]), 1))/60)
-    names = []
-    durations = []
-    names.append("Average")
-    durations.append(avg_track)
-    for i in df.tail(bars_per_graph)["duration"]:
-        durations.append(i/60)
-    for i in df.tail(bars_per_graph)["formatted_name"]:
-        names.append(i)
-    return names, durations, len(df), max(df["duration"]), min(df["duration"])
+	df.sort_values(by=["duration"], ascending=True, inplace=True)
+	avg_track = int((round(df["duration"].sum()/len(df["duration"]), 1))/60)
+	names = []
+	durations = []
+	names.append("Average")
+	durations.append(avg_track)
+	for i in df.tail(bars_per_graph)["duration"]:
+		durations.append(i/60)
+	for i in df.tail(bars_per_graph)["formatted_name"]:
+		names.append(i)
+	return names, durations, len(df), max(df["duration"]), min(df["duration"])
 
 
 def get_artist_info(df):
-    artist_dict = {}
-    for i in df["artists"]:
-        i = eval(i)
-        for artist in i:
-            if artist in artist_dict.keys():
-                artist_dict[artist] += 1
-            else:
-                artist_dict.update({artist: 1})
-    artist_dict = dict(sorted(artist_dict.items(), key=lambda item: item[1], reverse=True))
-    return artist_dict, len(artist_dict)
+	artist_dict = {}
+	artists = [eval(i)[0] if "str" in str(type(eval(i)[0])) else eval(i)[0] for i in df["artists"]]
+	for i in df["artists"]:
+		i = eval(i)
+		for artist in i:
+			if artist in artist_dict.keys():
+				artist_dict[artist] += 1
+			else:
+				artist_dict.update({artist: 1})
+	artist_dict = dict(sorted(artist_dict.items(), key=lambda item: item[1], reverse=True))
+	return artist_dict, len(artist_dict)
 
 
 def find_popular(artist_dict, artists_per_graph):
-    values_list = list(artist_dict.values())
-    popularity = {}
-    most_popular = []
-    uses = []
-    for i in values_list:
-        if i not in popularity.keys():
-            popularity.update({i: 1})
-        else:
-            popularity[i] += 1
-    use_nums = sorted(popularity.keys(), reverse=True)
-    # top = sorted(artist_dict, key=artist_dict.get, reverse=True)[:30]
-    # for i in top:
-    #     print(f"{i}:{artist_dict[i]}")
-    for key in use_nums:
-        for artist in artist_dict:
-            if artist_dict[artist] == key:
-                most_popular.append(artist)
-                uses.append(key)
-        if len(most_popular) >= artists_per_graph:
-            break
-    """       show average
-    # most_popular.append("Average")
-    # artist_use_sum = 0
-    # for key, val in popularity.items():
-    # 	artist_use_sum+=key*val
-    # uses.append(artist_use_sum/len(artist_dict))
-    """
-    return most_popular, uses
+	values_list = list(artist_dict.values())
+	popularity = {}
+	most_popular = []
+	uses = []
+	for i in values_list:
+		if i not in popularity.keys():
+			popularity.update({i: 1})
+		else:
+			popularity[i] += 1
+	use_nums = sorted(popularity.keys(), reverse=True)
+	# top = sorted(artist_dict, key=artist_dict.get, reverse=True)[:30]
+	# for i in top:
+	#     print(f"{i}:{artist_dict[i]}")
+	for key in use_nums:
+		for artist in artist_dict:
+			if artist_dict[artist] == key:
+				most_popular.append(artist)
+				uses.append(key)
+			if len(most_popular) >= artists_per_graph:
+				break
+	"""       show average
+	# most_popular.append("Average")
+	# artist_use_sum = 0
+	# for key, val in popularity.items():
+	# 	artist_use_sum+=key*val
+	# uses.append(artist_use_sum/len(artist_dict))
+	"""
+	return most_popular, uses
 
 
 def get_explicits(data):
-    explicits = {"Explicit": 0, "Clean": 0, "Unknown": 0}
-    for item in data["explicit"]:
-        if item == "True":
-            explicits["Explicit"] += 1
-        if item == "False":
-            explicits["Clean"] += 1
-        else:
-            explicits["Unknown"] += 1
-    return dict(sorted(explicits.items(), key=lambda item: item[1], reverse=True))
+	explicits = {"Explicit": 0, "Clean": 0, "Unknown": 0}
+	for item in data["explicit"]:
+		if item == "True":
+			explicits["Explicit"] += 1
+		if item == "False":
+			explicits["Clean"] += 1
+		else:
+			explicits["Unknown"] += 1
+	return dict(sorted(explicits.items(), key=lambda item: item[1], reverse=True))
 
 
 def get_genres():
-    with open(r"C:\\Users\etoom\python files\spotipy\genres.txt") as data_file:
-        data = eval(data_file.read())
-    return data
+	with open("genres.txt") as data_file:
+		data = eval(data_file.read())
+	return data
 
 
 def genre_data_organization(genres_per_graph):
-    data = get_genres()
-    # pprint(sorted(data.items(),key=lambda item: item[1], reverse=True))
-    values_list = list(data.values())
-    popularity = {}
-    most_popular = []
-    uses = []
-    for i in values_list:
-        if i not in popularity.keys():
-            popularity.update({i: 1})
-        else:
-            popularity[i] += 1
-    use_nums = sorted(popularity.keys(), reverse=True)
-    for key in use_nums:
-        for artist in data:
-            if data[artist] == key:
-                most_popular.append(artist.capitalize())
-                uses.append(key)
-        if len(most_popular) >= genres_per_graph:
-            break
-    return most_popular, uses, len(data)
+	data = get_genres()
+	values_list = list(data.values())
+	popularity = {}
+	most_popular = []
+	uses = []
+	for i in values_list:
+		if i not in popularity.keys():
+			popularity.update({i: 1})
+		else:
+			popularity[i] += 1
+	use_nums = sorted(popularity.keys(), reverse=True)
+	for key in use_nums:
+		for artist in data:
+			if data[artist] == key:
+				most_popular.append(artist.capitalize())
+				uses.append(key)
+		if len(most_popular) >= genres_per_graph:
+			break
+	return most_popular, uses, len(data)
 
 
 def covers(data):
-    names = data["formatted_name"]
-    copies = list(set([i for i in names if list(names).count(i) > 1]))
-    return len(copies)
+	names = data["formatted_name"]
+	copies = list(set([i for i in names if list(names).count(i) > 1]))
+	return len(copies)
 
 
 def release_date_data(data):
-    yrs = data["release_date"]
-    first = min(yrs)
-    last = max(yrs)
-    def to_decade(i): return int(str(i)[:-1]+"0")
-    yrs = sorted(list(map(to_decade, yrs)))
-    popularity = {}
-    for i in yrs:
-        if i not in popularity.keys():
-            popularity.update({i: 1})
-        else:
-            popularity[i] += 1
-    popularity = dict(sorted(popularity.items(), key=lambda item: item[0]))
-    return list(popularity.keys()), list(popularity.values()), last-first
+	yrs = data["release_date"]
+	first = min(yrs)
+	last = max(yrs)
+	def to_decade(i): return int(str(i)[:-1]+"0")
+	yrs = sorted(list(map(to_decade, yrs)))
+	popularity = {}
+	for i in yrs:
+		if i not in popularity.keys():
+			popularity.update({i: 1})
+		else:
+			popularity[i] += 1
+	popularity = dict(sorted(popularity.items(), key=lambda item: item[0]))
+	return list(popularity.keys()), list(popularity.values()), last-first
 
 
 def auto_pct(pct, allvalues):
-    absolute = int(pct / 100.*np.sum(allvalues))
-    return "{:.1f}%\n({:d})".format(pct, absolute)
+	absolute = int(pct / 100.*np.sum(allvalues))
+	return "{:.1f}%\n({:d})".format(pct, absolute)
 
 
 def get_podcast_data():
-    data = []
-    with open(r"C:\\Users\etoom\python files\spotipy\podcast_data.txt") as data_file:
-        raw_data = data_file.readlines()
-    for i in raw_data:
-        i = i.split("/")
-        data.append({
-            "name": i[2][:-1],
-            "duration": int(i[1]),
-            "show": i[0]
-        })
-    return data
+	data = []
+	with open("podcast_data.txt") as data_file:
+		raw_data = data_file.readlines()
+	for i in raw_data:
+		i = i.split("/")
+		data.append({
+			"name": i[2][:-1],
+			"duration": int(i[1]),
+			"show": i[0]
+		})
+	return data
 
 
 def get_podcast_duration(data):
-    total = 0
-    for i in data:
-        total += i["duration"]
-    return total
+	total = 0
+	for i in data:
+		total += i["duration"]
+	return total
 
 
 def get_show_frequency(data):
-    shows = []
-    show_dict = {}
-    for i in data:
-        shows.append(i["show"])
-    for i in shows:
-        show_dict.update({i: shows.count(i)})
-    return dict(sorted(show_dict.items(), key=lambda item: item[1], reverse=True))
+	shows = []
+	show_dict = {}
+	for i in data:
+		shows.append(i["show"])
+	for i in shows:
+		show_dict.update({i: shows.count(i)})
+	return dict(sorted(show_dict.items(), key=lambda item: item[1], reverse=True))
 
 
 def get_show_durations(data):
-    show_dict = {}
-    for i in data:
-        show = i["show"]
-        duration = int(i["duration"]/60)
-        if show in show_dict.keys():
-            show_dict[show] = show_dict[show] + duration
-        else:
-            show_dict.update({show: duration})
-    return dict(sorted(show_dict.items(), key=lambda item: item[1], reverse=True))
+	show_dict = {}
+	for i in data:
+		show = i["show"]
+		duration = int(i["duration"]/60)
+		if show in show_dict.keys():
+			show_dict[show] = show_dict[show] + duration
+		else:
+			show_dict.update({show: duration})
+	return dict(sorted(show_dict.items(), key=lambda item: item[1], reverse=True))
 
 
-data = read_data(divider)
+data = read_spotify_data(divider)
 fig = plt.figure()
 
 artist_dict, artist_num = get_artist_info(data)
@@ -260,46 +257,45 @@ print("")
 print(f"You have {len(podcast_data)} podcast episodes saved that are in total {podcast_hours} hours long.")
 print(f"Those episodes are from {len(shows.keys())} different shows.")
 
-for i in range(2):
-    print("")
+print("")
 
 show = True
 if True:
-    plt.yticks(fontsize=8)
-    plt.title("Most Liked Songs")
-    plt.xlabel("Number of Songs in Playlist")
-    plt.barh(popular_artists[::-1], artist_uses[::-1])
+	plt.yticks(fontsize=8)
+	plt.title("Most Liked Songs")
+	plt.xlabel("Number of Liked Songs")
+	plt.barh(popular_artists[::-1], artist_uses[::-1])
 elif True:
-    plt.yticks(fontsize=8)
-    plt.title("Longest Songs")
-    plt.xlabel("Song Duration (minutes)")
-    plt.barh(duration_names, durations)
+	plt.yticks(fontsize=8)
+	plt.title("Longest Songs")
+	plt.xlabel("Song Duration (minutes)")
+	plt.barh(duration_names, durations)
 elif True:
-    plt.yticks(fontsize=8)
-    plt.title("Most Popular Genres")
-    plt.xlabel("Number of Liked Genres")
-    plt.barh(popular_genres[::-1], genres_uses[::-1])
+	plt.yticks(fontsize=8)
+	plt.title("Most Popular Genres")
+	plt.xlabel("Number of Liked Genres")
+	plt.barh(popular_genres[::-1], genres_uses[::-1])
 elif True:
-    plt.title("Number of Explicit Songs(Aproximate)", pad=40)
-    plt.pie(x=explicits.values(), labels=explicits.keys(), radius=1.3,
-        autopct=lambda pct: auto_pct(pct, list(explicits.values())),)
+	plt.title("Number of Explicit Songs(Aproximate)", pad=40)
+	plt.pie(x=explicits.values(), labels=explicits.keys(), radius=1.3,
+			autopct=lambda pct: auto_pct(pct, list(explicits.values())),)
 elif True:
-    plt.title("Number of Covers(Aproximate)")
-    plt.pie(x=[len(data)-cover_num, cover_num], labels=["Originals", "Covers"], pctdistance=.85,
-        autopct=lambda pct: auto_pct(pct, [len(data)-cover_num, cover_num]))
+	plt.title("Number of Covers(Aproximate)")
+	plt.pie(x=[len(data)-cover_num, cover_num], labels=["Originals", "Covers"], pctdistance=.85,
+			autopct=lambda pct: auto_pct(pct, [len(data)-cover_num, cover_num]))
 elif True:
-    plt.title("Songs from Each Decade", pad=40, fontdict={'fontsize': 20})
-    plt.pie(x=release_nums, labels=release_decades, autopct=lambda pct: auto_pct(pct, release_nums),
-        pctdistance=.85, labeldistance=1.05)
+	plt.title("Songs from Each Decade", pad=40, fontdict={'fontsize': 20})
+	plt.pie(x=release_nums, labels=release_decades, autopct=lambda pct: auto_pct(pct, release_nums),
+			pctdistance=.85, labeldistance=1.05)
 elif True:
-    plt.title(f"Number of Episodes (total {sum(shows.values())})")
-    plt.pie(x=shows.values(), labels=shows.keys(),
-        autopct=lambda pct: auto_pct(pct, list(shows.values())))
+	plt.title(f"Number of Episodes (total {sum(shows.values())})")
+	plt.pie(x=shows.values(), labels=shows.keys(),
+			autopct=lambda pct: auto_pct(pct, list(shows.values())))
 elif True:
-    plt.title("Runtime (minutes)")
-    plt.pie(x=show_duration_dict.values(), labels=show_duration_dict.keys(),
-        autopct=lambda pct: auto_pct(pct, list(show_duration_dict.values())))
+	plt.title("Runtime (minutes)")
+	plt.pie(x=show_duration_dict.values(), labels=show_duration_dict.keys(),
+			autopct=lambda pct: auto_pct(pct, list(show_duration_dict.values())))
 else:
-    show = False
+	show = False
 if show:
-    plt.show()
+	plt.show()
