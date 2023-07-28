a99729931b400071ea298a9b7efe763d8b8aa95a
Evan Toomey <etoomey81@gmail.com>
2023-07-28 05:36:44 -0400

    made things faster + reduced amount of data stored

--- collect_data.py
+++ collect_data.py
@@ -12,6 +12,12 @@ start = time.time()
 
 load_dotenv()
 
+
+def iterations(length, max_request=50):
+	if length % max_request == 0:
+		return int((length/50)-1)
+	return int(str(length/50).split(".")[0])
+
 def format_song_name(song_name):
 	song_name = song_name.split(" (")[0]
 	song_name = song_name.split(" - ")[0]
@@ -21,49 +27,88 @@ def format_song_name(song_name):
 		song_name = song_name[:-1]
 	return song_name
 
+def format_artist(artist_dict):
+	artist_dict.pop("external_urls")
+	artist_dict.pop("href")
+	artist_dict.pop("uri")
+	return artist_dict
 
-def iterations(length, max_request=50):
-	if length % max_request == 0:
-		return int((length/50)-1)
-	return int(str(length/50).split(".")[0])
+def format_track(song):
+	if "added_at" in song.keys():
+		added = song["added_at"]
+		song = dict(song["track"])
+	else:
+		added = None
+	song["added_at"] = added
+	song["formatted_name"] = format_song_name(song["name"])
+	try:
+		song["album"].pop("available_markets")
+	except KeyError:
+		pass
+	song["album"].pop("external_urls")
+	song["album"].pop("href")
+	song["album"].pop("images")
+	song["album"].pop("uri")
+	song["album"]["artists"] = [format_artist(i) for i in song["album"]["artists"]]
+	song["artists"] = [format_artist(i) for i in song["artists"]]
+	try:
+		song.pop("available_markets")
+	except KeyError:
+		pass
+	song.pop("external_ids")
+	song.pop("external_urls")
+	song.pop("href")
+	song.pop("preview_url")
+	song.pop("uri")
+	song["artist_num"] = len(song["artists"])
+	return song
 
-def format_data(song_list):
-	song_list = dict(song_list)["items"]
-	for i in range(len(song_list)):
-		added = song_list[i]["added_at"]
-		song_list[i] = dict(song_list[i]["track"])
-		song_list[i]["added_at"] = added
-		song_list[i]["formatted_name"] = format_song_name(song_list[i]["name"])
-	return song_list
+def format_podcast(podcast):
+	if "added_at" in podcast.keys():
+		added = podcast["added_at"]
+		podcast = dict(podcast["episode"])
+	else:
+		added = None
+	podcast["added_at"] = added
+	podcast.pop("audio_preview_url")
+	podcast.pop("external_urls")
+	podcast.pop("href")
+	podcast.pop("html_description")
+	podcast.pop("images")
+	podcast.pop("is_externally_hosted")
+	podcast.pop("language")
+	podcast.pop("languages")
+	podcast.pop("uri")
+	podcast["show"] = format_podcast_show(podcast["show"])
+	return podcast
 
+def format_podcast_show(show):
+	show.pop("available_markets")
+	show.pop("copyrights")
+	show.pop("external_urls")
+	show.pop("href")
+	show.pop("html_description")
+	show.pop("images")
+	show.pop("is_externally_hosted")
+	show.pop("languages")
+	show.pop("uri")
+	return show
 
 def get_all_songs(spotify_client):
-	with open("tracks.json") as data_file:
-		data_file.write("")
 	raw_data = dict(spotify_client.current_user_saved_tracks(
 		limit=50, market="US", offset=0))
 	playlist_len = int(raw_data["total"])
-	data_list = format_data(raw_data)
+	data_list = [format_track(i) for i in raw_data["items"]]
 	for i in range(iterations(playlist_len)):
 		offset = i*50+51
 		raw_data = dict(spotify_client.current_user_saved_tracks(
 			limit=50, market="US", offset=offset))
-		data_list.extend(format_data(raw_data))
-	with open("tracks.json", "a") as data_file:
+		data_list.extend([format_track(i) for i in raw_data["items"]])
+	with open("tracks.json", "w") as data_file:
 		json.dump({"data":data_list}, data_file)
-
-def check_for_new_songs(spotify_client):
-	with open("tracks.json") as data_file:
-		data = dict(json.load(data_file))["data"]
-	saved_song_ids = []
-	for song in data:
-		saved_song_ids.append(song["id"])
-	saved_song_ids = list(set(saved_song_ids))
 
 
 def get_genres(spotify_client):
-	with open("genres.json", "w") as data_file:
-		data_file.write("")
 	with open("tracks.json") as data_file:
 		data = dict(json.load(data_file))["data"]
 	genre_list = []
@@ -82,14 +127,12 @@ def get_genres(spotify_client):
 
 
 def podcasts(sp):
-	with open("podcasts.json", "w") as data_file:
-		data_file.write("")
 	max_podcast_request = True
 	podcasts = []
 	n = 0
 	while max_podcast_request:
 		new_podcast_request = dict(sp.current_user_saved_episodes(offset = n))["items"]
-		podcasts.extend(new_podcast_request)
+		podcasts.extend([format_podcast(i) for i in new_podcast_request])
 		if len(new_podcast_request)<20:
 			max_podcast_request = False
 		n+=20
@@ -104,9 +147,9 @@ spotify_client = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, 
 	redirect_uri="http://www.evantoomey.com/", scope="user-library-read"))
 
 
-# threading.Thread(target=podcasts, args=(spotify_client,)).start()
+threading.Thread(target=podcasts, args=(spotify_client,)).start()
+threading.Thread(target=get_genres, args=(spotify_client,)).start()
 get_all_songs(spotify_client)
-# get_genres(spotify_client)
 
 
 total_time = round(time.time()-start, 0)
